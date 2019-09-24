5896842a0814485ed5f3bd166a53aa8933bc0be2
Marysia Pabich <marysia.pabich@gmail.com>
2019-09-24 22:32:31 +0200

    Create Product class and its instances

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,41 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
+      console.log('new Product: ', thisProduct);
+    }
+    renderInMenu () {
+      const thisProduct = this;
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
+    }
+  }
   const app = {
+    initMenu: function () {
+      const thisApp = this;
+      console.log('thisApp.data: ', thisApp.data);
+      for (let productData in thisApp.data.products) {
+        new Product (productData, thisApp.data.products[productData]);
+      }
+    },
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +94,10 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
+
     },
   };
 
