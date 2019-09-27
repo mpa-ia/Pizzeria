25b54b4356ef16300379acc34bc13028047cdf1d
Marysia Pabich <marysia.pabich@gmail.com>
2019-09-27 18:32:28 +0200

    Add imageWrapper method to getElements

--- src/js/script.js
+++ src/js/script.js
@@ -84,6 +84,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
     initAccordion () {
       const thisProduct = this;
