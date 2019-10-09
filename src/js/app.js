a1db7847b8273944819b1eb32f63f2b067ee67b6
Marysia Pabich <marysia.pabich@gmail.com>
2019-10-09 17:49:33 +0200

    Add functionality to switch between tabs

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,54 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /* run thisApp.activatePage () with that id  */
+        thisApp.activatePage(id);
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+
+  },
+  activatePage: function (pageId) {
+    const thisApp = this;
+    /* add class "Active" to matching pages, remove from non-matching */
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "Active" to matching links, remove from non-matching */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId);
+    }
+  },
   initMenu: function () {
     const thisApp = this;
     // console.log('thisApp.data: ', thisApp.data);
@@ -28,7 +74,7 @@ const app = {
     fetch(url)
       .then(rawResponse => rawResponse.json())
       .then(parsedResponse => {
-        console.log('parsedResponse: ', parsedResponse);
+        // console.log('parsedResponse: ', parsedResponse);
         /* save parsedResponse at thisApp.data.products */
         thisApp.data.products = parsedResponse;
         /* execute initMenu method */
@@ -36,10 +82,12 @@ const app = {
       });
 
 
-    console.log('thisApp.data: ', JSON.stringify(thisApp.data));
+    // console.log('thisApp.data: ', JSON.stringify(thisApp.data));
   },
   init: function(){
     const thisApp = this;
+
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
